a9ffd6683270fea09cf0973f9bd01e2df4f85eab
Sergey Bakhtoyarov <sergey.bakhtoyarov@gmail.com>
2018-10-14 23:46:56 +0200

    update

--- dist/util/secrets.js
+++ dist/util/secrets.js
@@ -4,6 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = __importDefault(require("./logger"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
+if (fs_1.default.existsSync(".env")) {
+    logger_1.default.debug("Using .env file to supply config environment variables");
+    dotenv_1.default.config({ path: ".env" });
+}
+else {
+    dotenv_1.default.config();
+}
 exports.ENVIRONMENT = process.env.NODE_ENV;
 const prod = exports.ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 exports.SESSION_SECRET = process.env["SESSION_SECRET"];
